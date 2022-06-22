c9e931165fa8e5a58d5286be00ddc708a51fb77f
Romain857 <romainmart.85@gmail.com>
2022-06-22 14:06:46 +0200

    test

--- components/btnCreate.vue
+++ components/btnCreate.vue
@@ -1,3 +1,9 @@
 <template>
-    <b-button href="/productCreate" variant="outline-dark"></i>New Product</b-button>
+    <b-button id="btn" href="/productCreate" variant="outline-dark"></i>New Product</b-button>
 </template>
+
+<style>
+    #btn{
+        margin-bottom: 1%
+    }
+</style>
